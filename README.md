f260d4a00aef58d856fafad642c5bc36d761b576
SourGaeko <66372255+SourGaeko@users.noreply.github.com>
2020-09-17 19:08:22 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# MogTube
+
+Music Lesson Videos for Students
+
+## Pages
+
+- [ ] Home
+- [ ] Join
+- [ ] Login
+- [ ] Logout
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
+- [ ] 
---
+++ middlewares.js
@@ -0,0 +1,17 @@
+import multer from "multer";
+import routes from "./routes";
+
+const multerVideo = multer({ dest: "uploads/videos/" });
+
+export const localsMiddleware = (req, res, next) => {
+  res.locals.siteName = "MogTube";
+  res.locals.routes = routes;
+  res.locals.user = {
+    isAuthenticated: false,
+    id: 1,
+  };
+  //    res.locals.routes = routes;
+  next();
+};
+
+export const uploadVideo = multerVideo.single("videoFile");
